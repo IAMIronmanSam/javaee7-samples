a5a82e8f97a43e2e74778dc8812f40e8bc69f1fc
Arun P. Gupta <arungup@arungup-mac.local>
2012-11-21 11:26:50 -0800

    Starting work on schema-gen sample for JPA2. Does not seem to be implemented in GlassFish 63 yet though.

---
+++ jpa2/schema-gen/src/main/java/org/sample/schemagen/Department.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sample.schemagen;
+
+/**
+ *
+ * @author arungup
+ */
+class Department {
+    
+}
