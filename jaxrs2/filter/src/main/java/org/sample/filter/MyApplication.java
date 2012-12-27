da1ddf978734c9383c4f0d192e9ad77f83cfaa8f
Arun Gupta <>
2012-12-27 12:35:31 -0800

    Added server filter to the list of resources

--- jaxrs2/filter/src/main/java/org/sample/filter/MyApplication.java
+++ jaxrs2/filter/src/main/java/org/sample/filter/MyApplication.java
@@ -39,6 +39,7 @@
  */
 package org.sample.filter;
 
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -47,5 +48,11 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("webresources")
 public class MyApplication extends Application {
-    
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        resources.add(org.sample.filter.MyResource.class);
+        resources.add(org.sample.filter.ServerLoggingFilter.class);
+        return resources;
+    }
 }
