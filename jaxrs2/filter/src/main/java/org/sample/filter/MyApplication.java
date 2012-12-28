ef1ce75235f026e6d47a8379ceb42b085fba88a7
Arun Gupta <>
2012-12-28 13:45:29 -0800

    Added a javadoc about a workaround for auto-scanning of resources in WAR

--- jaxrs2/filter/src/main/java/org/sample/filter/MyApplication.java
+++ jaxrs2/filter/src/main/java/org/sample/filter/MyApplication.java
@@ -48,6 +48,10 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("webresources")
 public class MyApplication extends Application {
+    
+    // Resource and filters need to be explicitly specified
+    // until http://java.net/jira/browse/JERSEY-1634 is fixed
+    
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
