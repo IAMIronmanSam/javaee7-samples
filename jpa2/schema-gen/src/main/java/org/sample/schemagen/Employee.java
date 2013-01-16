33b1745c2737bd68984f7f1a00bfc6a20547c0be
Arun Gupta <arungup@10.151.96.185>
2013-01-16 15:12:55 -0800

    Changed the copyright year to 2013

--- jpa2/schema-gen/src/main/java/org/sample/schemagen/Employee.java
+++ jpa2/schema-gen/src/main/java/org/sample/schemagen/Employee.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -45,12 +45,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  * @author Arun Gupta
  */
 @Entity
+@Table
 public class Employee implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
